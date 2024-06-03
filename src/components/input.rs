8f3e85cdae73935e199e0738d8ef7ca7bfe52866
danikxd <danmarek111@gmail.com>
2024-06-03 10:48:36 +0200

    issue11 - added caret for input

--- src/components/input.rs
+++ src/components/input.rs
@@ -3,20 +3,24 @@ use tui::layout::Rect;
 use tui::widgets::{Block, Borders, Paragraph};
 use tui::style::{Color, Modifier, Style};
 use tui::Frame;
-use crossterm::event::KeyCode;
+
+use crossterm::event::{KeyCode, KeyEvent,  Event};
+
+use tui_input::backend::crossterm::EventHandler;
+use tui_input::Input;
 
 use crate::ui::Component;
 
 pub struct InputComponent {
-    pub input: String,
-    pub scroll_x: u16,
+    pub input: Input,
+    
 }
 
 impl InputComponent {
     pub fn new() -> Self {
         Self {
-            input: String::new(),
-            scroll_x: 0,
+            input: Input::default(),
+            
         }
     }
 }
@@ -24,37 +28,31 @@ impl InputComponent {
 impl Component for InputComponent {
     fn draw<B: Backend>(&self, f: &mut Frame<B>, area: Rect, is_active: bool) {
         let block = Block::default()
-            .borders(Borders::ALL)
-            .title("Input")
-            .style(Style::default().fg(if is_active {
-                Color::Green
-            } else {
-                Color::White
-            }));
-        let paragraph = Paragraph::new(self.input.clone())
-            .block(block)
-            .style(Style::default().fg(Color::Yellow).add_modifier(Modifier::BOLD))
-            .scroll((0, self.scroll_x));
+        .borders(Borders::ALL)
+        .title("Input")
+        .style(Style::default().fg(if is_active {
+            Color::Green
+        } else {
+            Color::White
+        }));
+
+    let paragraph = Paragraph::new(self.input.value())
+        .block(block)
+        .style(Style::default().fg(Color::Yellow).add_modifier(Modifier::BOLD));
+
         f.render_widget(paragraph, area);
+
+        if is_active {
+            f.set_cursor(
+                area.x + self.input.visual_cursor() as u16 + 1,
+                area.y + 1,
+            )
+        }
     }
 
     fn keybinds(&mut self, key: KeyCode) {
-        match key {
-            KeyCode::Char(c) => {
-                self.input.push(c);
-            }
-            KeyCode::Backspace => {
-                self.input.pop();
-            }
-            KeyCode::Left => {
-                if self.scroll_x > 0 {
-                    self.scroll_x -= 1;
-                }
-            }
-            KeyCode::Right => {
-                self.scroll_x += 1;
-            }
-            _ => {}
-        }
+      
+                self.input.handle_event(&Event::Key(KeyEvent::new(key, crossterm::event::KeyModifiers::NONE)));
+          
     }
 }
